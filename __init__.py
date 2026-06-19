64e5e6c1ee8ed98016b17e7e70455d931b2ef899
Snehasis Satapathy <snehasissatapathy2005@gmail.com>
2026-06-19 15:47:38 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,14 @@
-"""Payment gateways (mock)."""
-from .gateway import PaymentGateway, PaymentResult, ScriptedGateway, FakeRandomGateway
+"""Tax calculators."""
+from .base import TaxCalculator, TaxContext, TaxBreakdown
+from .no_tax import NoTax
+from .vat import VATCalculator
+from .gst import GSTCalculator
 
-__all__ = ["PaymentGateway", "PaymentResult", "ScriptedGateway", "FakeRandomGateway"]
+__all__ = [
+    "TaxCalculator",
+    "TaxContext",
+    "TaxBreakdown",
+    "NoTax",
+    "VATCalculator",
+    "GSTCalculator",
+]
